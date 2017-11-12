d0a18ca5bc4af327efe81efa39ff92aae7020766
jaypee <jaypee@apple-no-MacBook-Pro.local>
2017-11-12 12:17:35 +0800

    Initial files.

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {   
+
+    details: {
+        company: "Erp Solutions", 
+        project_name: "TaskMe", 
+        project_icon: "fa-tasks"
+    },  
+    api: {
+        //login: "http://localhost/intranet-v3/published/login/validateLogin"
+        host: "localhost/", 
+        path: "archives/api/", 
+        port: 80
+    }
+     
+};  
